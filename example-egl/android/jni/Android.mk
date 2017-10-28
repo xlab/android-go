b69a958b537266a0845b36357f100caf77d745f2
Andrey Azarov <azarov.andrey@gmail.com>
2017-10-29 00:35:50 +0300

    First version

--- example-egl/android/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := example
-LOCAL_SRC_FILES := lib/libexample.so
-LOCAL_LDLIBS    := -llog -landroid
-
-include $(PREBUILT_SHARED_LIBRARY)
