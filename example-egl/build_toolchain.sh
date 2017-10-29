dcb5e7cc473602bc78e435f48d4f1d8a9b97bc97
Andrey Azarov <azarov.andrey@gmail.com>
2017-10-29 20:05:22 +0300

    Improve messages

--- example-egl/build_toolchain.sh
+++ example-egl/build_toolchain.sh
@@ -3,12 +3,12 @@ set -e
 
 ABIS=($(echo $1 | sed 's/,/ /g'))
 ANDROID_API=$2
-printf "Architectures to build: %s\nAndroid API version %s\n" "${ABIS[*]}" $ANDROID_API
+printf "Preparing toolchains for ABIs: %s Android API: %s\n" "${ABIS[*]}" $ANDROID_API
 
 
 TOOLCHAIN_ROOT_DIR=build_go/toolchain
 mkdir -p $TOOLCHAIN_ROOT_DIR
-printf "Toolchain root directory: %s\n" $TOOLCHAIN_ROOT_DIR
+printf "Toolchains root directory: %s\n" $TOOLCHAIN_ROOT_DIR
 for ABI in ${ABIS[*]}
 do
 
@@ -20,15 +20,16 @@ x86)
 ARCH="x86"
 ;;
 esac
-printf "Architecture: %s Application binary interface: %s\n" $ABI $ARCH
+printf "Preparing toolchain for ABI: %s arch: %s\n" $ABI $ARCH
 
 TOOLCHAIN_DIR="$TOOLCHAIN_ROOT_DIR/$ABI"
-printf "Toolchain dir: %s\n" "$TOOLCHAIN_DIR"
+printf "Cleaning toolchain dir: %s\n" "$TOOLCHAIN_DIR"
 rm -rf "$TOOLCHAIN_DIR"
-
+printf "Making standalone toolchain\n"
 set -x
 $ANDROID_HOME/ndk-bundle/build/tools/make_standalone_toolchain.py \
 		--api=$ANDROID_API --install-dir=$TOOLCHAIN_DIR \
 		--arch=$ARCH --stl libc++
 set +x
+printf "Standalone toolchain ready\n"
 done
