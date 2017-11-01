0b358f9bbe79e728bcbe850e8c23e65369a05b09
Andrey Azarov <azarov.andrey@gmail.com>
2017-11-02 00:48:44 +0300

    Multi-arch support, bash scripts refactoring

--- example-egl/build_go.sh
+++ example-egl/build_go.sh
@@ -4,7 +4,7 @@ set -ex
 
 ABIS=($(echo $1 | sed 's/,/ /g'))
 ANDROID_API=$2
-printf "Build Go sources using ABIs: %s Android API: %s Mode: %s\n" "${ABIS[*]}" $ANDROID_API "nomode"
+printf "Build Go sources using ABIs: %s Android API: %s\n" "${ABIS[*]}" "$ANDROID_API"
 
 TOOLCHAIN_ROOT_DIR=build_go/toolchain
 OUTPUT_ROOT_DIR=build_go/output
@@ -13,30 +13,41 @@ rm -rf "$OUTPUT_ROOT_DIR"
 
 for ABI in ${ABIS[*]}
 do
-
-TOOLCHAIN_DIR="$TOOLCHAIN_ROOT_DIR/$ABI"
+GOARCH=
+GOARM=
+CC=
+CXX=
+CGO_CFLAGS=
 
 case $ABI in
 armeabi-v7a)
-ARCH="arm"
-GOARCH=arm
+GOARCH="arm"
 GOARM=7
-CC="$TOOLCHAIN_DIR/bin/arm-linux-androideabi-gcc"
-CXX="$TOOLCHAIN_DIR/bin/arm-linux-androideabi-g++"
+CC="$TOOLCHAIN_ROOT_DIR/arm/bin/arm-linux-androideabi-gcc"
+CXX="$TOOLCHAIN_ROOT_DIR/arm/bin/arm-linux-androideabi-g++"
 CGO_CFLAGS="-march=armv7-a"
 ;;
 x86)
-ARCH="x86"
+GOARCH="386"
+GOARM=
+CC="$TOOLCHAIN_ROOT_DIR/x86/bin/i686-linux-android-gcc"
+CXX="$TOOLCHAIN_ROOT_DIR/x86/bin/i686-linux-android-g++"
+CGO_CFLAGS=
+;;
+*)
+continue
 ;;
 esac
 
 
-OUTPUT_DIR=$OUTPUT_ROOT_DIR/$ABI
-mkdir -p $OUTPUT_DIR
+OUTPUT_DIR="$OUTPUT_ROOT_DIR/$ABI"
+mkdir -p "$OUTPUT_DIR"
 
 cd ..
-CC=$(pwd)/android/$CC CXX=$(pwd)/android/$CXX CGO_ENABLED=1 CGO_CFLAGS=$CGO_CFLAGS GOOS=android GOARCH=$GOARCH GOARM=$GOARM \
-go build -i -pkgdir $(pwd)/android/$OUTPUT_DIR -buildmode=c-shared -o android/src/main/jniLibs/$ABI/libgomain.so
+CURRENT_DIR=$(pwd)
+CC="$CURRENT_DIR/android/$CC" CXX="$CURRENT_DIR/android/$CXX" CGO_ENABLED=1 \
+CGO_CFLAGS="$CGO_CFLAGS" GOOS=android GOARCH="$GOARCH" GOARM="$GOARM" \
+go build -i -pkgdir "$CURRENT_DIR/android/$OUTPUT_DIR" -buildmode=c-shared -o "android/src/main/jniLibs/$ABI/libgomain.so"
 cd android
 
 done
--- example-egl/build_toolchain.sh
+++ example-egl/build_toolchain.sh
@@ -8,11 +8,13 @@ printf "Preparing toolchains for ABIs: %s Android API: %s\n" "${ABIS[*]}" $ANDRO
 
 
 TOOLCHAIN_ROOT_DIR=build_go/toolchain
-mkdir -p $TOOLCHAIN_ROOT_DIR
-printf "Toolchains root directory: %s\n" $TOOLCHAIN_ROOT_DIR
+printf "Cleaning toolchains root directory: %s\n" "$TOOLCHAIN_ROOT_DIR"
+rm -rf "$TOOLCHAIN_ROOT_DIR"
+mkdir -p "$TOOLCHAIN_ROOT_DIR"
+
+declare -A ARCHS
 for ABI in ${ABIS[*]}
 do
-
 case $ABI in
 armeabi-v7a)
 ARCH="arm"
@@ -20,21 +22,27 @@ ARCH="arm"
 x86)
 ARCH="x86"
 ;;
+*)
+continue
+;;
 esac
-printf "Preparing toolchain for ABI: %s arch: %s\n" $ABI $ARCH
+printf "Toolchain will be created for arch: %s (ABI: %s)\n" $ARCH $ABI
 
-TOOLCHAIN_DIR="$TOOLCHAIN_ROOT_DIR/$ABI"
-printf "Cleaning toolchain dir: %s\n" "$TOOLCHAIN_DIR"
-rm -rf "$TOOLCHAIN_DIR"
-printf "Making standalone toolchain\n"
+ARCHS[$ARCH]=1
+done
+
+for ARCH in ${!ARCHS[*]}
+do
+
+TOOLCHAIN_DIR="$TOOLCHAIN_ROOT_DIR/$ARCH"
+printf "Making standalone toolchain for arch: %s\n" $ARCH
 set -x
-$ANDROID_HOME/ndk-bundle/build/tools/make_standalone_toolchain.py \
+"$ANDROID_HOME"/ndk-bundle/build/tools/make_standalone_toolchain.py \
 		--api=$ANDROID_API --install-dir=$TOOLCHAIN_DIR \
 		--arch=$ARCH --stl libc++
 # The command above includes the wrong headers due to a bug in the android sdk and/or cmake. The following two commands fix this issue.
 rm -rf $TOOLCHAIN_DIR/sysroot/usr
-cp -r  "$ANDROID_HOME/ndk-bundle/platforms/android-$ANDROID_API/arch-arm/usr" "$TOOLCHAIN_DIR/sysroot/usr"
-
+cp -r  "$ANDROID_HOME/ndk-bundle/platforms/android-$ANDROID_API/arch-$ARCH/usr" "$TOOLCHAIN_DIR/sysroot/usr"
 set +x
 printf "Standalone toolchain ready\n"
 done
